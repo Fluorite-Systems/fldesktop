3261d0326e02905fd3d209a15a00dd6a8d877ddd
Iamha111 <iamha111@yandex.ru>
2026-08-28 21:02:58 +0400

    Poweroff or reboot process now schould work when fldesktop was started by systemd, again, again.

--- src/fldesktop/include/os_manager.py
+++ src/fldesktop/include/os_manager.py
@@ -45,7 +45,13 @@ class OSManager:
         "Power off the system"
         self.prepare_logout()
         logging.info("Shutting down via systemctl, goodbye.")
-        subprocess.Popen(["systemctl", "poweroff", "--no-wall"])
+        subprocess.Popen([
+            "dbus-send", "--system", "--print-reply",
+            "--dest=org.freedesktop.login1",
+            "/org/freedesktop/login1",
+            "org.freedesktop.login1.Manager.PowerOff",
+            "boolean:true"
+        ], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         while True:
             time.sleep(1000)
     
@@ -53,7 +59,13 @@ class OSManager:
         "Reboot the system"
         self.prepare_logout()
         logging.info("Rebooting via systemctl, goodbye.")
-        subprocess.Popen(["systemctl", "reboot", "--no-wall"])
+        subprocess.Popen([
+            "dbus-send", "--system", "--print-reply",
+            "--dest=org.freedesktop.login1",
+            "/org/freedesktop/login1",
+            "org.freedesktop.login1.Manager.Reboot",
+            "boolean:true"
+        ], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         while True:
             time.sleep(1000)
     
