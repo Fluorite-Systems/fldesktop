2633438a95893734a8df2d9b5df03766d92f47dc
Iamha111 <iamha111@yandex.ru>
2026-09-03 22:25:48 +0400

    Removed unnecessary s

--- src/fldesktop/include/compositor/widgets/flayout.py
+++ src/fldesktop/include/compositor/widgets/flayout.py
@@ -6,6 +6,6 @@ class FLayout(Widget):
     def __init__(self, runner, name, props, parent):
         super().__init__(runner, name, props, parent)
         self.type = "flayout"
-        self.qlayout = FlowLayout(s)
+        self.qlayout = FlowLayout()
 
         self._setup()
