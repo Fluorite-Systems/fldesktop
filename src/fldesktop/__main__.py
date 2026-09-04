30efd3223cf9578bff7506a2822917d1e1669929
Iamha111 <iamha111@yandex.ru>
2026-09-04 22:47:19 +0400

    Removed logging to file becuz it does not work

--- src/fldesktop/__main__.py
+++ src/fldesktop/__main__.py
@@ -18,14 +18,11 @@
 from fldesktop.include.init import Init
 from fldesktop.include.args import ArgumentParser
 
-from pathlib import Path
-
 import logging
 import sys
 
 
 VERSION = "raw"
-LOGGING_DIR = Path("/system/logs")
 
 
 class Core:
@@ -38,18 +35,10 @@ class Core:
             print(VERSION)
             sys.exit()
 
-        if LOGGING_DIR.exists():
-            logging.basicConfig(
-                level=logging.DEBUG if self.ap.args.debug else logging.INFO,
-                format="%(asctime)s: %(filename)s: %(levelname)s: %(message)s",
-                filename=LOGGING_DIR / "fldesktop.log",
-                filemode="w"
-            )
-        else:
-            logging.basicConfig(
-                level=logging.DEBUG if self.ap.args.debug else logging.INFO,
-                format="%(asctime)s: %(filename)s: %(levelname)s: %(message)s"
-            )
+        logging.basicConfig(
+            level=logging.DEBUG if self.ap.args.debug else logging.INFO,
+            format="%(asctime)s: %(filename)s: %(levelname)s: %(message)s"
+        )
 
         logging.info(f"Welcome to fldesktop, version: {VERSION}")
 
