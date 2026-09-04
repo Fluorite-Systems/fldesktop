6203d6f180da44a882fccf9859b2a6ca072f7f5d
Iamha111 <iamha111@yandex.ru>
2026-09-04 18:56:56 +0400

    ImageView was adapted to new property system

--- src/fldesktop/include/compositor/widgets/imageview.py
+++ src/fldesktop/include/compositor/widgets/imageview.py
@@ -3,6 +3,8 @@ from PySide6.QtGui import QPixmap, QImageReader
 from PySide6.QtCore import Qt
 from fldesktop.include.compositor.widgets.base import Widget
 
+import base64
+
 
 class ImageView(Widget):
     def __init__(self, runner, name, props, parent):
@@ -10,48 +12,51 @@ class ImageView(Widget):
         self.type = "imageview"
         self.qwidget = QLabel()
 
-        self.callables = {
-            "set_image": self.set_image,
-            "set_source": self.set_source
+        self.base_props = {
+            "image": "",
+            "source": "",
+            "quality": "fast",
+            "keep_aspect_ratio": True,
+            "full_cover": False
         }
-
-        self._setup()
-
-        self.quality = "good"
-
-        if "quality" in props:
-            if props["quality"] == "fast":
-                self.quality = "fast"
 
         self.pixmap = QPixmap()
         self.qwidget.resizeEvent = self.resizeEvent
 
-    def set_image(self, image: str) -> None:
-        self.pixmap = QPixmap()
-        self.pixmap.loadFromData(base64.b64decode(image.encode()))
-        self.resizeEvent(None)
+        self._setup()
 
-    def set_source(self, source: str) -> None:
+    def apply_props(self):
+        super().apply_props()
 
-        if self.quality == "fast":
-            reader = QImageReader(source)
-            reader.setScaledSize(self.qwidget.size().scaled(
-                self.qwidget.size().width(),
-                self.qwidget.size().height(),
-                Qt.KeepAspectRatioByExpanding
-            ))
-            self.pixmap = QPixmap.fromImage(reader.read())
-        else:
-            self.pixmap = QPixmap(source)
+        if self.props["image"]:
+            self.pixmap = QPixmap()
+            self.pixmap.loadFromData(
+                base64.b64decode(self.props["image"].encode())
+            )
 
+        if self.props["source"]:
+            if self.props["quality"] == "fast":
+                reader = QImageReader(self.props["source"])
+                reader.setScaledSize(self.qwidget.size().scaled(
+                    self.qwidget.size().width(),
+                    self.qwidget.size().height(),
+                    Qt.KeepAspectRatioByExpanding
+                ))
+                self.pixmap = QPixmap.fromImage(reader.read())
+            else:
+                self.pixmap = QPixmap(self.props["source"])
+    
         self.resizeEvent(None)
 
     def resizeEvent(self, ev):
         self.qwidget.setPixmap(
             self.pixmap.scaled(
                 self.qwidget.size(),
-                Qt.KeepAspectRatioByExpanding,
-                Qt.FastTransformation if self.quality == "fast" else \
-                    Qt.SmoothTransformation
+                (Qt.KeepAspectRatioByExpanding if self.props["full_cover"] \
+                    else Qt.KeepAspectRatio) if \
+                        self.props["keep_aspect_ratio"] else \
+                            Qt.AspectRatioMode.IgnoreAspectRatio,
+                Qt.FastTransformation if self.props["quality"] == "fast" \
+                    else Qt.SmoothTransformation
             )
         )
